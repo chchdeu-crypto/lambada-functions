169c322121aab0a8ce903001300227083864d8d0
chaim <chchdeu@gmail.com>
2026-07-05 10:51:21 +0300

    mission_3

--- lambda_functions_exe.py
+++ lambda_functions_exe.py
@@ -3,4 +3,7 @@ manager_son_price=lambda price,son_of_m : price*0.8 if son_of_m==True else price
 
 #mission 2
 finel_price=lambda price,discount:price-discount
-print(finel_price(150,30))
+
+#mission 3
+full_name= lambda f_name,l_name: f"{f_name} {l_name}"
+print(full_name("chaim","deustch"))
