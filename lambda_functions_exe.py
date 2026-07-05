f865969ffc504ff1a04b3c2562b43cbf1cf92b57
chaim <chchdeu@gmail.com>
2026-07-05 10:49:03 +0300

    mission_2

--- lambda_functions_exe.py
+++ lambda_functions_exe.py
@@ -1,3 +1,6 @@
 #mission 1
 manager_son_price=lambda price,son_of_m : price*0.8 if son_of_m==True else price*1.17
-print(manager_son_price(150,False))
+
+#mission 2
+finel_price=lambda price,discount:price-discount
+print(finel_price(150,30))
